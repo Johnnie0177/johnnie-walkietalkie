cfaeae072180158ce2da9292d6999479a6ef2d98
Johnnie0177 <johnniegothired@gmail.com>
2025-05-30 19:10:25 -0700

    Added original code from source

---
+++ NearbyConnectionsWalkieTalkie/deploy.sh
@@ -0,0 +1,3 @@
+yo app/build/outputs/apk/automatic/debug/app-automatic-debug.apk
+adb -s ZY22K4GDZL install app/build/outputs/apk/automatic/debug/app-automatic-debug.apk
+adb -s 4200dd4caa12b40d install app/build/outputs/apk/automatic/debug/app-automatic-debug.apk
